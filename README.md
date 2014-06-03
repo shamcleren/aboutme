1a8600be554608ef364e93bdc3cddd613b45b4e1
Shamcle <siweidashu@gmail.com>
2014-06-04 01:33:31 +0800

    uuu

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 一个介绍自己的网站
 
-> angularjs + express + nodejs
+> angularjs
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,10 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+  .controller('index', ['$scope', function($scope) { 
+        $(".show").click(function(){
+            $(this).animate({marginLeft:-400} ,function(){
+                $(this).find("img").attr("src", "./img/me_60.png");
+            });
+        });
   }]);
